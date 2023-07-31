9d667c45bdcd7691de8679c7b23c77281016cd6e
Sam McCauley <sammccauley34@gmail.com>
2023-07-31 14:25:17 -0500

    Created birds reducer

---
+++ src/redux/reducers/birds.reducer.js
@@ -0,0 +1,11 @@
+const birdReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'SET_BIRDS':
+        console.log('action.payload is:', action.payload)
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export default birdReducer;
