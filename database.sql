757d48428515f4e12ee06764fb8ba780305fef2c
Sam McCauley <sammccauley34@gmail.com>
2023-07-31 13:57:10 -0500

    Set up DB/added sql to database file

--- database.sql
+++ database.sql
@@ -8,3 +8,29 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "my_collection" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL,
+	"bird_id" INT NOT NULL,
+	"location" VARCHAR (1000),
+	"date" DATE,
+	"time" TIME,
+	"notes" VARCHAR (1000)
+);
+
+CREATE TABLE "birds" (
+	"id" SERIAL PRIMARY KEY,
+	"common_name" VARCHAR (100),
+	"sci_name" VARCHAR (100),
+	"region" VARCHAR (100),
+	"family" VARCHAR (100),
+	"order" VARCHAR (100),
+	"image" VARCHAR (500)
+);
+
+CREATE TABLE "my_wishlist" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL,
+	"bird_id" INT NOT NULL
+);
